8751470f1a43ff1f4d3253d10951f0ae2c7c4225
feanlau <feanlau@outlook.com>
2024-07-31 21:13:30 +0800

    添加代码

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+import { createStore } from "redux";
+
+const initialState = []
+
+const reducer = function (state = initialState, action) {
+    switch (action.type) {
+        case "addBook":
+            console.log(state);
+            return []
+        case "delBook":
+            return []
+        default:
+            return [
+                ...state
+            ]
+            break;
+    }
+}
+
+
+const store = createStore(reducer);
+
+
 // console.log("Redux Hello!");
 const root = document.getElementById("app");
 const addBook = document.getElementById("addBook");
@@ -15,9 +38,31 @@ delBookBtn.innerText = "DEL BOOK"
 addBookBtn.addEventListener("click", addBookBtn)
 delBookBtn.addEventListener("click", delBookBtn)
 
+function* generateID() {
+    let id=0;
+    while(true) {
+        yield id++;
+    }
+} 
 
-function addBookFn(){}
-function delBookFn(){}
+function addBookFn() { 
+    const bookName = bookNameInput.value;
+    if(bookName) {
+        console.log(bookName);
+        const bookId = generateID();
+        bookNameInput.value = "";
+        const action = {
+            type: "addBook",
+            info: {
+                bookId: bookId,
+                bookName: bookName
+            } 
+        }
+
+        store.dispatch(action);
+    }
+}
+function delBookFn() { }
 
 addBook.appendChild(bookNameInput)
 addBook.appendChild(addBookBtn)
