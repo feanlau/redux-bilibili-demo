1552ef7b5dcfca32ac61a26ee37d96dbbd71730a
feanlau <feanlau@outlook.com>
2024-07-31 20:31:13 +0800

    添加代码

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log("Redux Hello!");
+
+const root = document.getElementById("app");
+const addBook = document.getElementById("addBook");
+const delBook = document.getElementById("delBook");
+const bookList = document.getElementById("bookList");
+
+const addBookBtn = document.createElement('button');
+const bookNameInput = document.createElement('input');
+const delBookBtn = document.createElement('button');
+const bookIdInput = document.createElement('input');
+
+addBookBtn.innerText = "ADD BOOK"
+delBookBtn.innerText = "DEL BOOK"
+
+addBookBtn.addEventListener("click", addBookBtn)
+delBookBtn.addEventListener("click", delBookBtn)
